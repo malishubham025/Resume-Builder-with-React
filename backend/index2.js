525d1bf79c6265a8b5f7c7a6852664dd50d49c39
shubham mali <98689609+malishubham025@users.noreply.github.com>
2024-09-14 23:34:06 +0530

    Delete backend directory

--- backend/index2.js
+++
@@ -1,6 +0,0 @@
-// const mongoose=require("mongoose");
-// mongoose.connect("mongodb+srv://malishubham025:MBIh4VWlsqfSPCWD@cluster0.cozcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
-//     console.log("connected !");
-// }).catch((err)=>{
-//     console.log(err);
-// })
